70be0d21adfe61a6d9785d78456538791da71619
Cipher1101 <cipher1101@outlook.com>
2026-08-31 22:35:16 -0300

    Brought Git up to latest version

--- src/extractmarkdown.py
+++ src/extractmarkdown.py
@@ -1,14 +1,13 @@
 import re
-from textnode import TextNode, TextType, text_node_to_html_node
 
 def extract_markdown_images(text: str) -> list[tuple]:
 
-    matches = re.findall(r"\[(.+)\]\((.+)\)", text)
-    
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
     return matches
 
 def extract_markdown_links(text: str) -> list[tuple]:
 
-    matches = re.findall(r"\[(.+)\]\((.+)\)", text)
-
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
     return matches
